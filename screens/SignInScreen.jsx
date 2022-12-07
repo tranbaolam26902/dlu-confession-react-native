c5917a15beb814e3ec603eca94a86f2be3419e7f
2011401 <2011401@dlu.edu.vn>
2022-12-07 16:27:24 +0700

    Add sign in & sign up screen ui

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -1,7 +1,124 @@
-import { Text } from 'react-native';
+import { useRef, useState } from 'react';
+import { StyleSheet, View, Text, Image, Pressable, Keyboard } from 'react-native';
 
-function SignInScreen() {
-    return <Text>Sign In Screen</Text>;
+import GlobalStyles from '../assets/styles/GlobalStyles';
+import images from '../assets/images';
+
+import Button from '../components/Button';
+import Input from '../components/Input';
+
+function SignInScreen({ navigation }) {
+    // Component's states
+    const [errorMessage, setErrorMessage] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Component's refs
+    const usernameInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    // Functions
+    const isValidSignInData = () => {
+        if (username === '' || password === '') {
+            setErrorMessage('Vui lòng điền đầy đủ thông tin!');
+            return false;
+        }
+        setErrorMessage('');
+        return true;
+    };
+
+    // Event handlers
+    const handleSignIn = () => {
+        if (!isValidSignInData()) return;
+        Keyboard.dismiss();
+        // call API
+    };
+    const handleSwitch = () => {
+        setErrorMessage('');
+        setUsername('');
+        setPassword('');
+        navigation.navigate('SignUp');
+    };
+
+    return (
+        <Pressable style={styles.wrapper} onPress={Keyboard.dismiss}>
+            <Image style={styles.logo} source={images.logo} />
+            <Text style={styles.title}>ĐĂNG NHẬP</Text>
+            {errorMessage !== '' ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+            <Input
+                ref={usernameInputRef}
+                style={{ marginBottom: 8 }}
+                label='Tên đăng nhập'
+                value={username}
+                setValue={setUsername}
+                blurOnSubmit={false}
+                returnKeyType='next'
+                onSubmitEditing={() => passwordInputRef.current.focus()}
+            />
+            <Input
+                ref={passwordInputRef}
+                label='Mật khẩu'
+                secureTextEntry
+                value={password}
+                setValue={setPassword}
+                onSubmitEditing={handleSignIn}
+            />
+            <Button style={{ marginVertical: 16 }} title='Đăng nhập' fluid accent onPress={handleSignIn} />
+            <View style={styles.forgotPassword}>
+                <Button title='Quên mật khẩu?' text />
+            </View>
+            <View style={styles.divider}></View>
+            <View style={styles.createAccount}>
+                <Text style={styles.text}>Chưa có tài khoản?</Text>
+                <Button title='Đăng ký' textAccent onPress={handleSwitch} />
+            </View>
+        </Pressable>
+    );
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        backgroundColor: GlobalStyles.colors.background,
+    },
+    logo: {
+        alignSelf: 'center',
+    },
+    title: {
+        marginTop: 16,
+        marginBottom: 48,
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: GlobalStyles.colors.secondary,
+        textAlign: 'center',
+    },
+    errorMessage: {
+        marginBottom: 24,
+        fontSize: 16,
+        color: GlobalStyles.colors.red,
+        textAlign: 'center',
+    },
+    forgotPassword: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    divider: {
+        marginVertical: 16,
+        height: 1,
+        backgroundColor: '#A7A7B1',
+    },
+    createAccount: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    text: {
+        marginRight: 4,
+        fontSize: 16,
+        color: GlobalStyles.colors.textColor,
+    },
+});
 
 export default SignInScreen;
--- screens/SignUpScreen.jsx
+++ screens/SignUpScreen.jsx
@@ -1,7 +1,165 @@
-import { Text } from 'react-native';
+import { useRef, useState } from 'react';
+import { StyleSheet, View, Text, Image, Pressable, Keyboard } from 'react-native';
 
-function SignUpScreen() {
-    return <Text>Sign Up Screen</Text>;
+import GlobalStyles from '../assets/styles/GlobalStyles';
+import images from '../assets/images';
+
+import Button from '../components/Button';
+import Input from '../components/Input';
+
+function SignUpScreen({ navigation }) {
+    // Component's states
+    const [errorMessage, setErrorMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassowrd] = useState('');
+
+    // Component's refs
+    const emailInputRef = useRef();
+    const usernameInputRef = useRef();
+    const nicknameInputRef = useRef();
+    const passwordInputRef = useRef();
+    const confirmPasswordInputRef = useRef();
+
+    // Functions
+    const isValidSignUpData = () => {
+        if (email === '' || username === '' || nickname === '' || password === '' || confirmPassword === '') {
+            setErrorMessage('Vui lòng điền đầy đủ thông tin!');
+            return false;
+        }
+        if (password !== confirmPassword) {
+            setErrorMessage('Mật khẩu xác thực không trùng khớp!');
+            return false;
+        }
+        setErrorMessage('');
+        return true;
+    };
+
+    // Event handlers
+    const handleSignUp = () => {
+        if (!isValidSignUpData()) return;
+        Keyboard.dismiss();
+        // call API
+    };
+    const handleSwitch = () => {
+        setErrorMessage('');
+        setEmail('');
+        setUsername('');
+        setNickname('');
+        setPassword('');
+        setConfirmPassowrd('');
+        navigation.goBack();
+    };
+
+    return (
+        <Pressable style={styles.wrapper} onPress={Keyboard.dismiss}>
+            <Image style={styles.logo} source={images.logo} />
+            <Text style={styles.title}>ĐĂNG KÝ</Text>
+            {errorMessage !== '' ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+            <Input
+                ref={emailInputRef}
+                style={{ marginBottom: 8 }}
+                label='Email'
+                value={email}
+                setValue={setEmail}
+                blurOnSubmit={false}
+                returnKeyType='next'
+                onSubmitEditing={() => usernameInputRef.current.focus()}
+            />
+            <Input
+                ref={usernameInputRef}
+                style={{ marginBottom: 8 }}
+                label='Tên đăng nhập'
+                value={username}
+                setValue={setUsername}
+                blurOnSubmit={false}
+                returnKeyType='next'
+                onSubmitEditing={() => nicknameInputRef.current.focus()}
+            />
+            <Input
+                ref={nicknameInputRef}
+                style={{ marginBottom: 8 }}
+                label='Tên hiển thị'
+                value={nickname}
+                setValue={setNickname}
+                blurOnSubmit={false}
+                returnKeyType='next'
+                onSubmitEditing={() => passwordInputRef.current.focus()}
+            />
+            <Input
+                ref={passwordInputRef}
+                label='Mật khẩu'
+                secureTextEntry
+                value={password}
+                setValue={setPassword}
+                blurOnSubmit={false}
+                returnKeyType='next'
+                onSubmitEditing={() => confirmPasswordInputRef.current.focus()}
+            />
+            <Input
+                ref={confirmPasswordInputRef}
+                style={{ marginBottom: 8 }}
+                label='Nhập lại mật khẩu'
+                secureTextEntry
+                value={confirmPassword}
+                setValue={setConfirmPassowrd}
+                onSubmitEditing={handleSignUp}
+            />
+            <Button style={{ marginVertical: 16 }} title='Đăng ký' fluid accent onPress={handleSignUp} />
+            <View style={styles.divider}></View>
+            <View style={styles.createAccount}>
+                <Text style={styles.text}>Đã có tài khoản?</Text>
+                <Button title='Đăng nhập' textAccent onPress={handleSwitch} />
+            </View>
+        </Pressable>
+    );
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        backgroundColor: GlobalStyles.colors.background,
+    },
+    logo: {
+        alignSelf: 'center',
+    },
+    title: {
+        marginTop: 16,
+        marginBottom: 32,
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: GlobalStyles.colors.secondary,
+        textAlign: 'center',
+    },
+    errorMessage: {
+        marginBottom: 16,
+        fontSize: 16,
+        color: GlobalStyles.colors.red,
+        textAlign: 'center',
+    },
+    forgotPassword: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    divider: {
+        marginVertical: 16,
+        height: 1,
+        backgroundColor: '#A7A7B1',
+    },
+    createAccount: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    text: {
+        marginRight: 4,
+        fontSize: 16,
+        color: GlobalStyles.colors.textColor,
+    },
+});
 
 export default SignUpScreen;
