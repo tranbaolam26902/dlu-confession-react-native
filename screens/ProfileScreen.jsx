4b8bae1314a304b915492b7bbb6b6cce6aee24d7
tranbaolam <tranbaolam26902@gmail.com>
2022-12-09 18:22:07 +0700

    Add default layout

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,7 +1,61 @@
-import { Text } from 'react-native';
+import { useCallback, useState } from 'react';
+import { StyleSheet, ScrollView, Text, StatusBar, RefreshControl } from 'react-native';
 
-function ProfileScreen() {
-    return <Text>Profile Screen</Text>;
+import { useStore } from '../store';
+import GlobalStyles from '../assets/styles/GlobalStyles';
+
+import HeaderBar from '../components/header';
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+    },
+});
+
+function ProfileScreen({ navigation, route }) {
+    // Global states
+    const [states, dispatch] = useStore();
+    const { apiURL } = states;
+
+    // Component's states
+    const [refreshing, setRefreshing] = useState(false);
+
+    // Functions
+    const wait = (timeout) => {
+        return new Promise((resolve) => setTimeout(resolve, timeout));
+    };
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        let time = new Date();
+        // Call API
+        // fetch(`${apiURL}/token`, {
+        //     method: 'POST',
+        //     body: `grant_type=password&username=Admin&password=Admin#123`,
+        // })
+        //     .then((response) => response.json())
+        //     .then((response) => {
+        //         let time = new Date() - time;        // set time for refreshing
+        //         if (response.access_token) {
+        //             console.log(time);
+        //         }
+        //     });
+        wait(1000).then(() => setRefreshing(false));
+    }, []);
+
+    return (
+        <>
+            <StatusBar backgroundColor={GlobalStyles.colors.white} barStyle={'dark-content'} />
+            <HeaderBar />
+            <ScrollView
+                style={styles.wrapper}
+                decelerationRate={'normal'}
+                showsVerticalScrollIndicator={false}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+            >
+                <Text>Profile Screen</Text>
+            </ScrollView>
+        </>
+    );
 }
 
 export default ProfileScreen;
