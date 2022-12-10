cce4f2ab78ccd1ff314d2f9024d49588cd4c61ee
tranbaolam <tranbaolam26902@gmail.com>
2022-12-10 16:04:33 +0700

    Remove tab components

--- components/tabbar/TabBarLabel.jsx
+++
@@ -1,7 +0,0 @@
-import { Text } from 'react-native';
-
-function TabBarLabel({ text, color }) {
-    return <Text style={{ fontSize: 12, color: color }}>{text}</Text>;
-}
-
-export default TabBarLabel;
