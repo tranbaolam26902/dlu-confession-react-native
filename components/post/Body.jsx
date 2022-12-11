62543a729fa1a124baad21907e9442e885fad594
tranbaolam <tranbaolam26902@gmail.com>
2022-12-11 13:41:16 +0700

    Revert

--- components/post/Body.jsx
+++ components/post/Body.jsx
@@ -3,16 +3,12 @@ import { StretchInX } from 'react-native-reanimated';
 import images from '../../assets/images';
 import GlobalStyles from '../../assets/styles/GlobalStyles';
 import { useStore } from '../../store';
-import { useNavigation } from '@react-navigation/native';
 
 function PostBody({ data }) {
     const [states, dispatch] = useStore();
     const { apiURL, imageURL } = states;
-
-    const navigation = useNavigation();
-
     return (
-        <Pressable onPress={() => navigation.navigate('PostDetail', { data: data })}>
+        <View>
             <Text numberOfLines={2} style={styles.title}>
                 {data.Title}
             </Text>
@@ -26,7 +22,7 @@ function PostBody({ data }) {
                     );
                 })}
             </View>
-        </Pressable>
+        </View>
     );
 }
 
