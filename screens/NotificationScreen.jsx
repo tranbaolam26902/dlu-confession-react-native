9e782a2d16bf05d4584e4d3766dc0bc07371b12c
tranbaolam <tranbaolam26902@gmail.com>
2022-12-12 13:08:47 +0700

    Edit NotificationScreen

--- screens/NotificationScreen.jsx
+++ screens/NotificationScreen.jsx
@@ -1,59 +1,113 @@
-import { useCallback, useState } from 'react';
-import { StyleSheet, ScrollView, Text, StatusBar, RefreshControl } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, StatusBar, RefreshControl, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { useStore } from '../store';
 import GlobalStyles from '../assets/styles/GlobalStyles';
 
 import HeaderBar from '../components/header';
+import Notification from '../components/notification';
+import Button from '../components/Button';
+import Empty from '../components/Empty';
 
 const styles = StyleSheet.create({
     wrapper: {
         flex: 1,
+        backgroundColor: GlobalStyles.colors.background,
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: GlobalStyles.colors.secondary,
     },
 });
 
-function NotificationScreen({ navigation, route }) {
+function NotificationScreen() {
     // Global states
     const [states, dispatch] = useStore();
     const { apiURL } = states;
 
     // Component's states
     const [refreshing, setRefreshing] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+    const [token, setToken] = useState();
 
     // Functions
-    const wait = (timeout) => {
-        return new Promise((resolve) => setTimeout(resolve, timeout));
+    const getToken = async () => {
+        await AsyncStorage.getItem('@token', (error, item) => {
+            setToken(item);
+        });
     };
-    const onRefresh = useCallback(() => {
+    const getNotifications = () => {
+        if (token)
+            fetch(`${apiURL}/api/UserNotify/index`, {
+                headers: {
+                    Authorization: token,
+                },
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    setNotifications(response);
+                });
+    };
+
+    // Event handlers
+    const onRefresh = async () => {
         setRefreshing(true);
-        let time = new Date();
-        // Call API
-        // fetch(`${apiURL}/token`, {
-        //     method: 'POST',
-        //     body: `grant_type=password&username=Admin&password=Admin#123`,
-        // })
-        //     .then((response) => response.json())
-        //     .then((response) => {
-        //         let time = new Date() - time;        // set time for refreshing
-        //         if (response.access_token) {
-        //             console.log(time);
-        //         }
-        //     });
-        wait(1000).then(() => setRefreshing(false));
-    }, []);
+        fetch(`${apiURL}/api/UserNotify/index`, {
+            headers: {
+                Authorization: token,
+            },
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                setNotifications(response);
+                setRefreshing(false);
+            });
+    };
+    const handleReadAll = () => {
+        fetch(`${apiURL}/api/UserNotify/ReadAll`, {
+            headers: {
+                Authorization: token,
+            },
+        })
+            .then((response) => response.json())
+            .then((responseNotifications) => {
+                setNotifications(responseNotifications);
+            });
+    };
+
+    useEffect(() => {
+        getToken();
+        getNotifications();
+    }, [token]);
 
     return (
         <>
             <StatusBar backgroundColor={GlobalStyles.colors.white} barStyle={'dark-content'} />
             <HeaderBar />
-            <ScrollView
+            <FlatList
                 style={styles.wrapper}
                 decelerationRate={'normal'}
                 showsVerticalScrollIndicator={false}
                 refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-            >
-                <Text>Notification Screen</Text>
-            </ScrollView>
+                data={notifications}
+                renderItem={({ item }) => <Notification data={item} />}
+                ListHeaderComponent={() => (
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Thông báo</Text>
+                        <Button title='Đánh dấu tất cả là đã đọc' text onPress={handleReadAll} />
+                    </View>
+                )}
+                ListEmptyComponent={() => <Empty text='Không có thông báo nào gần đây' />}
+            />
         </>
     );
 }
