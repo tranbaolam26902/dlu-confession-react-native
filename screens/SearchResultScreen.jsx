f1c3784083648f92b575d96a5df6a87aeb3c2db2
tranbaolam <tranbaolam26902@gmail.com>
2022-12-13 21:22:16 +0700

    Refactor

--- screens/SearchResultScreen.jsx
+++ screens/SearchResultScreen.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { FlatList, View } from 'react-native';
+
 import Empty from '../components/Empty';
 import Post from '../components/post';
 
@@ -14,16 +15,13 @@ function SearchResultScreen({ route }) {
 
     return (
         <View>
-            {data.length !== 0 ? (
-                <FlatList
-                    decelerationRate={'normal'}
-                    data={data}
-                    showsVerticalScrollIndicator={false}
-                    renderItem={({ item }) => <Post data={item} />}
-                />
-            ) : (
-                <Empty text='Không tìm thấy bài viết!' />
-            )}
+            <FlatList
+                decelerationRate={'normal'}
+                data={data}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => <Empty text='Không tìm thấy bài viết!' />}
+                renderItem={({ item }) => <Post data={item} />}
+            />
         </View>
     );
 }
